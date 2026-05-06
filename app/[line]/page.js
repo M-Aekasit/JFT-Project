1c90583c372bf0a8bfecf05941480d1da82ac4e7
Aekasit Makham <aekasit@csithai.com>
2026-05-06 17:26:49 +0700

    Update path for display

--- app/[line]/page.js
+++ app/[line]/page.js
@@ -1,5 +1,5 @@
-import { redirect } from 'next/navigation';
+import { redirect } from "next/navigation";
 
 export default function LineHomePage({ params }) {
-  redirect(`/${params.line}/dashboard`);
+  redirect(`/${params.line}/line-maintenance`);
 }
--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
-import { redirect } from 'next/navigation';
+import { redirect } from "next/navigation";
 
 export default function HomePage() {
-  redirect('/line1/dashboard');
+  redirect("/line1/line-maintenance");
 }
